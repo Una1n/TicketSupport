4573aab0075ff1301aaa9b93822271fbacca0947
Unain <jvdstoel@pm.me>
2023-08-03 11:08:43 +0200

    Added UserSeeder for default user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Jerry',
+            'email' => 'admin@admin.com',
+        ]);
+    }
+}
