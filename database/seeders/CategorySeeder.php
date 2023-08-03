227b65cbed828135e948a6ccaaa4abf32b2c43dc
Unain <jvdstoel@pm.me>
2023-08-03 16:02:41 +0200

    Changed categories to be for customers

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,8 +10,8 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::factory()->create(['name' => 'Uncategorized']);
-        Category::factory()->create(['name' => 'Windows']);
+        Category::factory()->create(['name' => 'Payment']);
         Category::factory()->create(['name' => 'Profile']);
-        Category::factory()->create(['name' => 'Network']);
+        Category::factory()->create(['name' => 'Shipping']);
     }
 }
