eeb172143628ea5007057e39915d085099c3eff9
Unain <jvdstoel@pm.me>
2023-08-03 16:21:51 +0200

    Added CategorySeeder to migration for defaults

--- database/migrations/2023_08_03_134244_create_categories_table.php
+++ database/migrations/2023_08_03_134244_create_categories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\CategorySeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,9 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        $seeder = new CategorySeeder();
+        $seeder->run();
     }
 
     public function down(): void
