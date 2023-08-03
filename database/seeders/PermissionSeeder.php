c5ab2a05760ab881272824381dbcebef1c19bebb
Unain <jvdstoel@pm.me>
2023-08-03 15:41:54 +0200

    Added some more permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -17,19 +17,12 @@ class PermissionSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // super admin
         $permissions[] = Permission::create(['name' => 'access dashboard']);
+        $permissions[] = Permission::create(['name' => 'access logs']);
         $permissions[] = Permission::create(['name' => 'manage tickets']);
         $permissions[] = Permission::create(['name' => 'manage users']);
-        // $permissions[] = Permission::create(['name' => 'manage news']);
-        // $permissions[] = Permission::create(['name' => 'manage competitions']);
-        // $permissions[] = Permission::create(['name' => 'manage teams']);
-        // $permissions[] = Permission::create(['name' => 'manage players']);
-        // $permissions[] = Permission::create(['name' => 'manage users']);
-        // $permissions[] = Permission::create(['name' => 'manage minimumcaramboles']);
-        // $permissions[] = Permission::create(['name' => 'manage moyennes']);
-        // $permissions[] = Permission::create(['name' => 'view logs']);
-        // $permissions[] = Permission::create(['name' => 'view address']);
+        $permissions[] = Permission::create(['name' => 'manage categories']);
+        $permissions[] = Permission::create(['name' => 'manage labels']);
 
         $adminRole = Role::create(['name' => 'Admin']);
         $adminRole->givePermissionTo($permissions);
