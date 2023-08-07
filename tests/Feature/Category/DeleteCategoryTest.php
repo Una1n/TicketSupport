6edd40cac2126afabdd24b927442f671e808a43a
Unain <jvdstoel@pm.me>
2023-08-07 14:55:12 +0200

    Added Delete Category Test

---
+++ tests/Feature/Category/DeleteCategoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Livewire\Categories\ListCategory;
+use App\Models\Category;
+use App\Models\User;
+
+beforeEach(function () {
+    login();
+});
+
+it('can delete a category', function () {
+    $category = Category::factory()->create();
+    $categoryID = $category->id;
+
+    Livewire::test(ListCategory::class)
+        ->call('deleteCategory', $category);
+
+    $category = Category::whereId($categoryID)->first();
+    expect($category)->toBeNull();
+});
+
+it('is only allowed to reach this endpoint when logged in as admin', function () {
+    login(User::factory()->create());
+
+    $category = Category::factory()->create();
+
+    // TODO: Not working yet on livewire 3 beta 7
+    // Livewire::test(ListCategory::class)
+    //     ->call('deleteCategory', $category)
+    //     ->assertForbidden();
+})->todo();
