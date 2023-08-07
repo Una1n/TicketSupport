4266eb2d35c89fd9fbe36401b56997a3f6b07ae1
Unain <jvdstoel@pm.me>
2023-08-07 15:37:19 +0200

    Created Status Component for Messages

---
+++ resources/views/components/status.blade.php
@@ -0,0 +1,23 @@
+@if (session()->has('status'))
+    <div id="alert-border-3" x-data="{ show: true }" x-show="show"
+        class="mb-4 flex items-center border-t-4 border-green-300 bg-green-50 p-4 text-green-800" role="alert">
+        <svg class="h-4 w-4 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 20 20">
+            <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <div class="ml-3 text-sm font-medium">
+            {{ session('status') }}
+        </div>
+        <button type="button" @click="show = false"
+            class="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-green-50 p-1.5 text-green-500 hover:bg-green-200 focus:ring-2 focus:ring-green-400"
+            data-dismiss-target="#alert-border-3" aria-label="Close">
+            <span class="sr-only">Dismiss</span>
+            <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+        </button>
+    </div>
+@endif
