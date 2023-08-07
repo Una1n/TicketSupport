e4167a18362a2bb3f87f7ab2ecefdc9f718b1c70
Unain <jvdstoel@pm.me>
2023-08-07 14:32:14 +0200

    Added Category Tests (List/Add)

---
+++ tests/Feature/Category/AddCategoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Livewire\Categories\CreateCategory;
+use App\Models\Category;
+use App\Models\User;
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+    login();
+});
+
+it('has component on create page', function () {
+    get(route('categories.create'))
+        ->assertSeeLivewire(CreateCategory::class)
+        ->assertOk();
+});
+
+it('can create a new category', function () {
+    Livewire::test(CreateCategory::class)
+        ->set('name', 'Test Name')
+        ->call('save');
+
+    $category = Category::whereName('Test Name')->first();
+    expect($category)->not->toBeNull();
+});
+
+it('validates name is required', function () {
+    Livewire::test(CreateCategory::class)
+        ->set('name', '')
+        ->call('save')
+        ->assertHasErrors('name');
+});
+
+it('validates name is unique', function () {
+    Category::factory()->create(['name' => 'not unique']);
+
+    Livewire::test(CreateCategory::class)
+        ->set('name', 'not unique')
+        ->call('save')
+        ->assertHasErrors('name');
+});
+
+it('is only allowed to reach this endpoint when logged in as admin', function () {
+    login(User::factory()->create());
+
+    // TODO: Not working yet on livewire 3 beta 7
+    // Livewire::test(CreateCategory::class)
+    //     ->set('name', 'test')
+    //     ->call('save')
+    //     ->assertForbidden();
+
+    get(route('categories.create'))
+        ->assertForbidden();
+});
---
+++ tests/Feature/Category/ShowCategoriesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Livewire\Categories\ListCategory;
+use App\Models\Category;
+use App\Models\User;
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+    login();
+});
+
+it('has component on index page', function () {
+    get(route('categories.index'))
+        ->assertSeeLivewire(ListCategory::class)
+        ->assertOk();
+});
+
+it('can show a list of categories', function () {
+    $categories = Category::factory(3)->create();
+
+    Livewire::test(ListCategory::class)
+        ->assertSee($categories[0]->name)
+        ->assertSee($categories[1]->name)
+        ->assertSee($categories[2]->name);
+});
+
+it('is only allowed to reach this endpoint when logged in as admin', function () {
+    login(User::factory()->create());
+
+    get(route('categories.index'))
+        ->assertForbidden();
+});
