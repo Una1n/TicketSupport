ac2c8e0f7476c2df64169c1acab1c37dd1e21af1
Unain <jvdstoel@pm.me>
2023-08-08 13:41:30 +0200

    Added Authorization for menu items

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -38,62 +38,70 @@
                             Tickets
                         </a>
                     </li>
-                    <li @class([
-                        'mb-2 rounded hover:bg-gray-800 hover:shadow',
-                        'bg-gray-800 shadow' => request()->routeIs('users.*'),
-                    ])>
-                        <a href="{{ route('users.index') }}" wire:navigate
-                            class="inline-block h-full w-full px-3 py-2 font-bold text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                            </svg>
-                            Users
-                        </a>
-                    </li>
-                    <li @class([
-                        'mb-2 rounded hover:bg-gray-800 hover:shadow',
-                        'bg-gray-800 shadow' => request()->routeIs('logs.*'),
-                    ])>
-                        <a href="#" class="inline-block h-full w-full px-3 py-2 font-bold text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="-mt-1 mr-2 inline-block h-6 w-6"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                            </svg>
-                            Ticket Logs
-                        </a>
-                    </li>
-                    <li @class([
-                        'mb-2 rounded hover:bg-gray-800 hover:shadow',
-                        'bg-gray-800 shadow' => request()->routeIs('categories.*'),
-                    ])>
-                        <a href="{{ route('categories.index') }}" wire:navigate
-                            class="inline-block h-full w-full px-3 py-2 font-bold text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
-                            </svg>
-                            Categories
-                        </a>
-                    </li>
-                    <li @class([
-                        'mb-2 rounded hover:bg-gray-800 hover:shadow',
-                        'bg-gray-800 shadow' => request()->routeIs('labels.*'),
-                    ])>
-                        <a href="{{ route('labels.index') }}" wire:navigate
-                            class="inline-block h-full w-full px-3 py-2 font-bold text-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
-                            </svg>
-                            Labels
-                        </a>
-                    </li>
+                    @can('manage users')
+                        <li @class([
+                            'mb-2 rounded hover:bg-gray-800 hover:shadow',
+                            'bg-gray-800 shadow' => request()->routeIs('users.*'),
+                        ])>
+                            <a href="{{ route('users.index') }}" wire:navigate
+                                class="inline-block h-full w-full px-3 py-2 font-bold text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                    stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                                </svg>
+                                Users
+                            </a>
+                        </li>
+                    @endcan
+                    @can('access logs')
+                        <li @class([
+                            'mb-2 rounded hover:bg-gray-800 hover:shadow',
+                            'bg-gray-800 shadow' => request()->routeIs('logs.*'),
+                        ])>
+                            <a href="#" class="inline-block h-full w-full px-3 py-2 font-bold text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="-mt-1 mr-2 inline-block h-6 w-6"
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                </svg>
+                                Ticket Logs
+                            </a>
+                        </li>
+                    @endcan
+                    @can('manage categories')
+                        <li @class([
+                            'mb-2 rounded hover:bg-gray-800 hover:shadow',
+                            'bg-gray-800 shadow' => request()->routeIs('categories.*'),
+                        ])>
+                            <a href="{{ route('categories.index') }}" wire:navigate
+                                class="inline-block h-full w-full px-3 py-2 font-bold text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                    stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
+                                </svg>
+                                Categories
+                            </a>
+                        </li>
+                    @endcan
+                    @can('manage labels')
+                        <li @class([
+                            'mb-2 rounded hover:bg-gray-800 hover:shadow',
+                            'bg-gray-800 shadow' => request()->routeIs('labels.*'),
+                        ])>
+                            <a href="{{ route('labels.index') }}" wire:navigate
+                                class="inline-block h-full w-full px-3 py-2 font-bold text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                    stroke="currentColor" class="-mt-1 mr-2 inline-block h-6 w-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                                </svg>
+                                Labels
+                            </a>
+                        </li>
+                    @endcan
                     <hr class="my-4">
                     <li class="mb-2 mt-4 rounded hover:bg-gray-800 hover:shadow">
                         <form method="post" action="{{ route('logout') }}">
