9c8dd9f576db74ff27d8ce847bdf9de924e875d5
Unain <jvdstoel@pm.me>
2023-08-15 16:34:20 +0200

    Updated Logs to show when no causer

--- resources/views/livewire/activity-logs/show-logs.blade.php
+++ resources/views/livewire/activity-logs/show-logs.blade.php
@@ -1,29 +1,36 @@
 <div>
-    @foreach ($logs as $log)
-        <div class="mt-4 rounded-lg border border-orange-600 bg-orange-200 px-4 py-2 shadow-md">
-            <div class="mb-1 text-xs">
-                @if ($log->event === 'created')
-                    {{ $log->created_at->diffForHumans() }} by
-                    <span class="font-semibold">{{ $log->causer->name }}</span>
-                @else
-                    {{ $log->updated_at->diffForHumans() }} by
-                    <span class="font-semibold">{{ $log->causer->name }}</span>
-                @endif
-            </div>
-            <div class="col-span-5">
-                @if ($log->event === 'created')
-                    Ticket Created
-                @else
-                    Ticket Updated:</br>
-                    @foreach ($log->changes->get('attributes') as $key => $attribute)
-                        @if ($key === 'agent.name')
-                            <p><span class="text-gray-500">Agent Assigned = </span> {{ $attribute }}</p>
-                        @else
-                            <p><span class="text-gray-500">{{ ucfirst($key) }} = </span> {{ $attribute }}</p>
+    <div class="mt-4 text-xl font-bold">Logs</div>
+    <div class="mb-4">
+        @foreach ($logs as $log)
+            <div class="mt-2 rounded-lg border border-orange-600 bg-orange-200 px-4 py-2 shadow-md">
+                <div class="mb-1 text-xs">
+                    @if ($log->event === 'created')
+                        {{ $log->created_at->diffForHumans() }}
+                        @if ($log->causer)
+                            <span class="font-semibold"> by {{ $log->causer->name }}</span>
                         @endif
-                    @endforeach
-                @endif
+                    @else
+                        {{ $log->updated_at->diffForHumans() }}
+                        @if ($log->causer)
+                            <span class="font-semibold"> by {{ $log->causer->name }}</span>
+                        @endif
+                    @endif
+                </div>
+                <div>
+                    @if ($log->event === 'created')
+                        Ticket Created
+                    @else
+                        Ticket Updated:</br>
+                        @foreach ($log->changes->get('attributes') as $key => $attribute)
+                            @if ($key === 'agent.name')
+                                <p><span class="text-gray-500">Agent Assigned = </span> {{ $attribute }}</p>
+                            @else
+                                <p><span class="text-gray-500">{{ ucfirst($key) }} = </span> {{ $attribute }}</p>
+                            @endif
+                        @endforeach
+                    @endif
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
