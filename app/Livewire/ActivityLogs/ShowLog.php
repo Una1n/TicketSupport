b88cf8e0e5bffd27492ae9f43f007583ae49529a
Unain <jvdstoel@pm.me>
2023-08-16 18:32:54 +0200

    Initial Show Log component

---
+++ app/Livewire/ActivityLogs/ShowLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ActivityLogs;
+
+use Livewire\Component;
+
+class ShowLog extends Component
+{
+    public function render()
+    {
+        return view('livewire.activity-logs.show-log');
+    }
+}
