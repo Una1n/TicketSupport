0c97d3fac33cbc045a4bf259b14841d6b447cf15
Unain <jvdstoel@pm.me>
2025-04-23 16:54:54 +0200

    Fixed PHPStan issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -28,7 +28,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -45,9 +45,6 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    /**
-     * @return HasMany<Ticket,User>
-     */
     public function tickets(): HasMany
     {
         return $this->hasMany(Ticket::class);
