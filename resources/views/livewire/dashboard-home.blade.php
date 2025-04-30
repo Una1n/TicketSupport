3022401d6ead4f4d312aa9c42651febb511f7038
Unain <jvdstoel@pm.me>
2025-04-30 16:13:13 +0200

    Updated Home with proper theme colors

--- resources/views/livewire/dashboard-home.blade.php
+++ resources/views/livewire/dashboard-home.blade.php
@@ -1,9 +1,10 @@
 <div>
-    <div class="mb-5 text-2xl font-bold">Home</div>
+    <!-- <div class="mb-5 text-2xl font-bold">Home</div> -->
+    <x-mary-header title="Home" separator />
     <div class="flex gap-10">
-        <div class="w-full max-w-lg rounded-lg bg-white shadow">
+        <div class="w-full max-w-lg rounded-lg bg-base-300 shadow">
             <div class="flex gap-5 p-4">
-                <div class="flex items-center justify-center text-gray-400">
+                <div class="flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                         stroke="currentColor" class="h-10 w-10">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -11,16 +12,16 @@
                     </svg>
                 </div>
                 <div>
-                    <h3 class="text-lg font-medium text-gray-500">Open Tickets</h3>
-                    <p class="mt-1 text-2xl font-bold text-red-600">
-                        {{ $openTickets }} <span class="text-gray-300">/ {{ $totalTickets }}</span>
+                    <h3 class="text-lg font-medium">Open Tickets</h3>
+                    <p class="mt-1 text-2xl font-bold text-error">
+                        {{ $openTickets }} <span class="text-base-content">/ {{ $totalTickets }}</span>
                     </p>
                 </div>
             </div>
         </div>
-        <div class="w-full max-w-lg rounded-lg bg-white shadow">
+        <div class="w-full max-w-lg rounded-lg bg-base-300 shadow">
             <div class="flex gap-5 p-4">
-                <div class="flex items-center justify-center text-gray-400">
+                <div class="flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                         stroke="currentColor" class="h-10 w-10">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -28,9 +29,9 @@
                     </svg>
                 </div>
                 <div>
-                    <h3 class="text-lg font-medium text-gray-500">Closed Tickets</h3>
-                    <p class="mt-1 text-2xl font-bold text-green-600">
-                        {{ $closedTickets }} <span class="text-gray-300">/ {{ $totalTickets }}</span>
+                    <h3 class="text-lg font-medium">Closed Tickets</h3>
+                    <p class="mt-1 text-2xl font-bold text-success">
+                        {{ $closedTickets }} <span class="text-base-content">/ {{ $totalTickets }}</span>
                     </p>
                 </div>
             </div>
