257e54db0450af3587a23365d41de9c8a77220d1
Unain <jvdstoel@pm.me>
2025-05-06 15:43:27 +0200

    Fixed Table Pagination Style

--- resources/views/livewire/activity-logs/list-log.blade.php
+++ resources/views/livewire/activity-logs/list-log.blade.php
@@ -1,7 +1,6 @@
 <div>
     <x-mary-header title="Ticket Activity Logs" separator />
     <div class="card bg-base-100 p-5 pt-2 shadow-xs">
-        <x-mary-table :headers="$headers" :rows="$logs" :sort-by="$sortBy" with-pagination>
-        </x-mary-table>
+        <x-mary-table :headers="$headers" :rows="$logs" :sort-by="$sortBy" with-pagination />
     </div>
 </div>
