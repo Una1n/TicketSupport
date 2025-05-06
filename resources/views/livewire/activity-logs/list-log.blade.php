3f57e044cc3b9bd2585c6070a796ec0a2973ad13
Unain <jvdstoel@pm.me>
2025-05-06 16:55:26 +0200

    Updated Log List/Show UI

--- resources/views/livewire/activity-logs/list-log.blade.php
+++ resources/views/livewire/activity-logs/list-log.blade.php
@@ -1,6 +1,10 @@
 <div>
     <x-mary-header title="Ticket Activity Logs" separator />
     <div class="card bg-base-100 p-5 pt-2 shadow-xs">
-        <x-mary-table :headers="$headers" :rows="$logs" :sort-by="$sortBy" with-pagination />
+        <x-mary-table :headers="$headers" :rows="$logs" :sort-by="$sortBy" :link="route('logs.show', ['log' => '[id]'])" with-pagination>
+            @scope('cell_created_at', $log)
+                {{ $log->created_at->diffForHumans() }}
+            @endscope
+        </x-mary-table>
     </div>
 </div>
--- resources/views/livewire/activity-logs/show-log.blade.php
+++ resources/views/livewire/activity-logs/show-log.blade.php
@@ -1,27 +1,14 @@
-<div>
-    <x-status />
-    <div class="mb-5 text-2xl font-bold">Log Information</div>
-    <div class="rounded-lg border border-gray-200 bg-white shadow-md">
-        <div class="grid grid-cols-6 gap-6 p-4">
-            <div class="font-bold">Ticket Title</div>
-            <div class="col-span-5">
-                {{ ucfirst($log->subject->title) }}
-            </div>
-            <div class="font-bold">Description</div>
-            <div class="col-span-5">
-                {{ ucfirst($log->description) }}
-            </div>
-            <div class="font-bold">Caused By</div>
-            <div class="col-span-5">
-                {{ $log->causer?->name }}
-            </div>
-            <div class="font-bold">Created</div>
-            <div class="col-span-5">
-                {{ $log->created_at->diffForHumans() }}
-            </div>
-            <hr class="col-span-full">
+<div class="max-w-5xl">
+    <x-mary-header title="Activity Log" separator />
+    <x-mary-card title="{{ ucfirst($log->subject->title) }}" subtitle="{{ ucfirst($log->description) }}" shadow separator>
+        <x-slot:menu>
+            <x-mary-badge value="{{ $log->created_at->diffForHumans() }}" class="badge-primary badge-soft" />
+        </x-slot:menu>
+        <div class="grid grid-cols-5 gap-6">
+            <div class="font-bold">Caused by:</div>
+            <x-mary-badge value="{{ $log->causer?->name }}" class="col-span-4 badge-primary" />
             <div class="font-bold">Attributes:</div>
-            <pre class="col-span-5 overflow-auto">{{ json_encode(json_decode($log->changes, true), JSON_PRETTY_PRINT) }}</pre>
+            <pre class="col-span-4 overflow-auto">{{ json_encode(json_decode($log->changes, true), JSON_PRETTY_PRINT) }}</pre>
         </div>
-    </div>
+    </x-mary-card>
 </div>
