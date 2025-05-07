946d0363c39c05882c02fb39c5d05394d6b5ec79
Unain <jvdstoel@pm.me>
2025-05-07 17:02:53 +0200

    Fixed Logout button not working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,8 +48,12 @@
                     <x-mary-list-item :item="$user" value="name" sub-value="email" no-separator no-hover
                         class="-mx-2 !-my-2 rounded">
                         <x-slot:actions>
-                            <x-mary-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff"
-                                no-wire-navigate link="/logout" />
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-mary-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff"
+                                    no-wire-navigate type="submit" />
+                            </form>
                         </x-slot:actions>
                     </x-mary-list-item>
 
