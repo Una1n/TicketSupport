d4b44c20c9d529085a9e2e05f0ff90ba65149441
Unain <jvdstoel@pm.me>
2025-05-08 15:20:41 +0200

    Updated Show Log UI

--- resources/views/livewire/activity-logs/show-log.blade.php
+++ resources/views/livewire/activity-logs/show-log.blade.php
@@ -1,18 +1,34 @@
 <div class="max-w-5xl">
     <x-mary-header title="Ticket Activity Log" separator />
     <div class="flex flex-col gap-6">
-        <x-mary-card title="{{ ucfirst($log->description) }}" shadow separator>
+        <x-mary-card title="Ticket" shadow separator>
             <x-slot:menu>
+                @if ($log->description === 'updated')
+                    <x-mary-icon name="o-pencil-square" class="text-warning" />
+                @elseif ($log->description === 'created')
+                    <x-mary-icon name="o-plus-circle" class="text-success" />
+                @elseif ($log->description === 'deleted')
+                    <x-mary-icon name="o-trash" class="text-error" />
+                @endif
                 <x-mary-badge value="{{ $log->created_at->diffForHumans() }}" class="badge-primary badge-soft" />
             </x-slot:menu>
             <div class="flex flex-col gap-6">
-                <div>
-                    <div class="font-bold">Title:</div>
-                    {{ ucfirst($log->subject->title) }}
+                <div class="font-bold wrap-break-word">
+                    @if ($log->description === 'deleted')
+                        {{ ucfirst(json_decode($log->properties, true)['old']['title']) }}
+                    @else
+                        {{ ucfirst($log->subject->title) }}
+                    @endif
                 </div>
                 @if ($log->causer)
                     <div>
-                        <div class="font-bold">Caused by:</div>
+                        @if ($log->description === 'updated')
+                            <div class="font-bold">Edited by:</div>
+                        @elseif ($log->description === 'created')
+                            <div class="font-bold">Created by:</div>
+                        @elseif ($log->description === 'deleted')
+                            <div class="font-bold">Deleted by:</div>
+                        @endif
                         @if ($role->name === 'Regular')
                             <div>{{ $log->causer->name }} <x-mary-badge value="{{ $role->name }}"
                                     class="badge-accent badge-soft" /></div>
