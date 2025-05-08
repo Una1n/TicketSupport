68344f1ab89a2cf5b87396c5cb55e27fc6f0db3b
Unain <jvdstoel@pm.me>
2025-05-08 12:56:12 +0200

    Updated Show Log UI

--- app/Livewire/ActivityLogs/ShowLog.php
+++ app/Livewire/ActivityLogs/ShowLog.php
@@ -12,6 +12,14 @@ class ShowLog extends Component
 
     public function render(): View
     {
-        return view('livewire.activity-logs.show-log');
+        $user = $this->log->causer;
+        $role = null;
+        if ($user) {
+            $role = $user->roles()->first();
+        }
+
+        return view('livewire.activity-logs.show-log', [
+            'role' => $role
+        ]);
     }
 }
--- resources/views/livewire/activity-logs/show-log.blade.php
+++ resources/views/livewire/activity-logs/show-log.blade.php
@@ -1,14 +1,34 @@
 <div class="max-w-5xl">
-    <x-mary-header title="Activity Log" separator />
-    <x-mary-card title="{{ ucfirst($log->subject->title) }}" subtitle="{{ ucfirst($log->description) }}" shadow separator>
-        <x-slot:menu>
-            <x-mary-badge value="{{ $log->created_at->diffForHumans() }}" class="badge-primary badge-soft" />
-        </x-slot:menu>
-        <div class="grid grid-cols-5 gap-6">
-            <div class="font-bold">Caused by:</div>
-            <x-mary-badge value="{{ $log->causer?->name }}" class="col-span-4 badge-primary" />
-            <div class="font-bold">Attributes:</div>
-            <pre class="col-span-4 overflow-auto">{{ json_encode(json_decode($log->changes, true), JSON_PRETTY_PRINT) }}</pre>
-        </div>
-    </x-mary-card>
+    <x-mary-header title="Ticket Activity Log" separator />
+    <div class="flex flex-col gap-6">
+        <x-mary-card title="{{ ucfirst($log->description) }}" shadow separator>
+            <x-slot:menu>
+                <x-mary-badge value="{{ $log->created_at->diffForHumans() }}" class="badge-primary badge-soft" />
+            </x-slot:menu>
+            <div class="flex flex-col gap-6">
+                <div>
+                    <div class="font-bold">Title:</div>
+                    {{ ucfirst($log->subject->title) }}
+                </div>
+                @if ($log->causer)
+                    <div>
+                        <div class="font-bold">Caused by:</div>
+                        @if ($role->name === 'Regular')
+                            <div>{{ $log->causer->name }} <x-mary-badge value="{{ $role->name }}"
+                                    class="badge-accent badge-soft" /></div>
+                        @elseif ($role->name === 'Agent')
+                            <div>{{ $log->causer->name }} <x-mary-badge value="{{ $role->name }}"
+                                    class="badge-primary badge-soft" /></div>
+                        @elseif ($role->name === 'Admin')
+                            <div>{{ $log->causer->name }} <x-mary-badge value="{{ $role->name }}"
+                                    class="badge-warning badge-soft" /></div>
+                        @endif
+                    </div>
+                @endif
+            </div>
+        </x-mary-card>
+        <x-mary-card title="Properties" shadow separator>
+            <pre class="overflow-auto">{{ json_encode(json_decode($log->changes, true), JSON_PRETTY_PRINT) }}</pre>
+        </x-mary-card>
+    </div>
 </div>
