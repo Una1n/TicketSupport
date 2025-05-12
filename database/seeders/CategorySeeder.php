6b0192bd057eb5592763a04396b78c2714dd108e
Unain <jvdstoel@pm.me>
2025-05-12 16:19:35 +0200

    Uncategorized is a useless category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -9,7 +9,7 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        Category::factory()->create(['name' => 'Uncategorized']);
+        Category::factory()->create(['name' => 'Visual']);
         Category::factory()->create(['name' => 'Payment']);
         Category::factory()->create(['name' => 'Profile']);
         Category::factory()->create(['name' => 'Shipping']);
