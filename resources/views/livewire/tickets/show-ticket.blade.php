128cc1563a9dc89d805e30a084229e711e91fdfa
Unain <jvdstoel@pm.me>
2025-05-13 13:24:04 +0200

    Updated Show Ticket UI

--- resources/views/livewire/tickets/show-ticket.blade.php
+++ resources/views/livewire/tickets/show-ticket.blade.php
@@ -1,78 +1,87 @@
 <div x-data="{ isShowingLogs: false, isShowingComments: true }">
-    <x-status />
-    <div class="mb-5 text-2xl font-bold">Ticket Information</div>
-    <div class="rounded-lg border border-gray-200 bg-white shadow-md">
-        <div class="grid grid-cols-6 gap-6 p-4">
-            <div class="font-bold">Title</div>
-            <div class="col-span-5">{{ ucfirst($ticket->title) }}</div>
-            <div class="font-bold">Status</div>
-            <div class="col-span-5">
+    <x-mary-header title="Ticket" separator>
+        <x-slot:actions>
+            <x-mary-button label="Edit" icon="o-pencil-square" class="text-warning btn-ghost" responsive />
+            <x-mary-button label="Delete" icon="o-trash" class="text-error btn-ghost" responsive />
+        </x-slot:actions>
+    </x-mary-header>
+    <div class="flex flex-col gap-6">
+        <x-mary-card title="{{ ucfirst($ticket->title) }}" shadow separator>
+            <x-slot:menu>
                 @if ($ticket->status === 'closed')
-                    <span
-                        class="inline-flex items-center gap-1 rounded-full bg-green-200 px-2 py-1 text-sm font-semibold text-green-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
-                            <path fill-rule="evenodd"
-                                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        {{ ucfirst($ticket->status) }}
-                    </span>
+                    <x-mary-icon name="o-check" class="text-success" />
+                    <span class="text-success hidden lg:block">Closed</span>
                 @else
-                    <span class="inline-flex items-center text-sm font-semibold text-red-600">
-                        {{ ucfirst($ticket->status) }}
-                    </span>
+                    <x-mary-icon name="o-exclamation-circle" class="text-error" />
+                    <span class="text-error hidden lg:block">Open</span>
                 @endif
-            </div>
-            <div class="font-bold">Priority</div>
-            <div class="col-span-5">
-                <span @class([
-                    'inline-flex items-center gap-1 rounded-full px-2 py-1 text-xs font-semibold',
-                    'bg-green-200 text-green-700' => $ticket->priority === 'low',
-                    'bg-yellow-200 text-yellow-700' => $ticket->priority === 'medium',
-                    'bg-red-200 text-red-700' => $ticket->priority === 'high',
-                ])>
-                    {{ ucfirst($ticket->priority) }}
-                </span>
-            </div>
-            <div class="font-bold">Description</div>
-            <div class="col-span-5">{{ $ticket->description }}</div>
-            <div class="font-bold">Categories</div>
-            <div class="col-span-5">
-                @foreach ($ticket->categories as $category)
-                    <span class="rounded-full bg-blue-200 px-2 py-1 text-xs text-blue-800">{{ $category->name }}</span>
-                @endforeach
-            </div>
-            <div class="font-bold">Labels</div>
-            <div class="col-span-5">
-                @foreach ($ticket->labels as $label)
-                    <span
-                        class="rounded-full bg-orange-200 px-2 py-1 text-xs text-orange-800">{{ $label->name }}</span>
-                @endforeach
-            </div>
-            <div class="font-bold">Created By</div>
-            <div class="col-span-5">{{ $ticket->user->name }}</div>
-            <div class="font-bold">Agent Assigned</div>
-            <div class="col-span-5">{{ $ticket->agent->name ?? '' }}</div>
-            <div class="font-bold">Attachment(s)</div>
-            <div class="col-span-5">
-                @foreach ($ticket->getMedia('attachments') as $attachment)
-                    <a href="{{ $attachment->getUrl() }}"
-                        class="text-blue-600 hover:underline">{{ $attachment->file_name }}</a>
-                    @if ($loop->iteration !== $loop->count)
-                        ,
+            </x-slot:menu>
+            <div class="flex flex-col gap-6">
+                <div class="wrap-break-word">
+                    <div>{{ ucfirst($ticket->description) }}</div>
+                </div>
+                <div>
+                    <div class="font-bold">Priority</div>
+                    @if ($ticket->priority === 'low')
+                        <x-mary-badge value="Low" class="badge-soft badge-accent" />
+                    @elseif ($ticket->priority === 'medium')
+                        <x-mary-badge value="Medium" class="badge-soft badge-warning" />
+                    @else
+                        <x-mary-badge value="High" class="badge-soft badge-error" />
                     @endif
-                @endforeach
+                </div>
+                <div>
+                    <div class="font-bold">Attachment(s)</div>
+                    <div>
+                        @forelse ($ticket->getMedia('attachments') as $attachment)
+                            <a href="{{ $attachment->getUrl() }}"
+                                class="text-blue-600 hover:underline">{{ $attachment->file_name }}</a>
+                            @if ($loop->iteration !== $loop->count)
+                                ,
+                            @endif
+                        @empty
+                            <span class="text-base-content/30">No Attachments Found</span>
+                        @endforelse
+                    </div>
+                </div>
             </div>
+        </x-mary-card>
+        <div class="flex flex-col lg:flex-row justify-stretch gap-6">
+            <x-mary-card title="Classification" class="grow" shadow separator>
+                <div class="flex flex-col gap-6">
+                    <div>
+                        <div class="font-bold">Categories</div>
+                        @foreach ($ticket->categories as $category)
+                            <x-mary-badge value="{{ $category->name }}" class="badge-soft badge-primary" />
+                        @endforeach
+                    </div>
+                    <div>
+                        <div class="font-bold">Labels</div>
+                        @foreach ($ticket->labels as $label)
+                            <x-mary-badge value="{{ $label->name }}" class="badge-soft badge-secondary" />
+                        @endforeach
+                    </div>
+                </div>
+            </x-mary-card>
+            <x-mary-card title="Users" class="grow" shadow separator>
+                <div class="flex flex-col gap-6">
+                    <div>
+                        <div class="font-bold">Created By</div>
+                        <div class="text-base-content/50">{{ $ticket->user->name }}</div>
+                    </div>
+                    <div>
+                        <div class="font-bold">Assigned Agent</div>
+                        <div class="text-base-content/50">{{ $ticket->agent->name }}</div>
+                    </div>
+                </div>
+            </x-mary-card>
         </div>
-    </div>
-    <div class="flex gap-5">
-        <button @click="isShowingLogs = !isShowingLogs" x-text="isShowingLogs ? 'Hide Logs' : 'Show Logs'"
-            class="mt-4 rounded-lg border border-green-500 bg-green-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-green-700 hover:bg-green-700 focus:ring focus:ring-green-200 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300">
-        </button>
-        <button @click="isShowingComments = !isShowingComments"
-            x-text="isShowingComments ? 'Hide Comments' : 'Show Comments'"
-            class="mt-4 rounded-lg border border-green-500 bg-green-500 px-5 py-2.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-green-700 hover:bg-green-700 focus:ring focus:ring-green-200 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300">
-        </button>
+        <div class="flex gap-5">
+            <x-mary-button label="Hide Logs" x-text="isShowingLogs ? 'Hide Logs' : 'Show Logs'"
+                @click="isShowingLogs = !isShowingLogs" class="btn-primary btn-dash" />
+            <x-mary-button label="Hide Comments" x-text="isShowingComments ? 'Hide Comments' : 'Show Comments'"
+                @click="isShowingComments = !isShowingComments" class="btn-primary btn-dash" />
+        </div>
     </div>
     <div x-show="isShowingComments" wire:transition>
         <livewire:comments.show-comments :ticket="$ticket" />
